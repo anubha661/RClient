acab9d70e3e9a6be094850e1e06bff1a71ff9d82
anubha <anubhajain@gmail.com>
2018-03-11 23:41:05 +0530

    Client1

---
+++ src/main/java/com/messageSeeker/ClientApplication.java
@@ -0,0 +1,14 @@
+package com.messageSeeker;
+
+
+
+
+public class ClientApplication {
+
+    public static void main(String args[])
+
+    {
+        MessageController controller=new MessageController();
+        controller.start();
+    }
+}
